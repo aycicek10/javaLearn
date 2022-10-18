046a5a6f4290770cd1c105d06a8612316c62008f
Ahmet Aycicek <aaycicek09@gmail.com>
2022-10-18 18:12:44 +0300

    new

---
+++ src/practice/day09/Q01_Arrays.java
@@ -0,0 +1,19 @@
+package practice.day09;
+
+public class Q01_Arrays {
+    public static void main(String[] args) {
+
+//Type a method to check if an int Array is symmetrical or not.
+
+        int[] arr={1,2,3,2,1};
+
+        boolean flg=
+
+
+
+
+
+
+
+    }
+}
