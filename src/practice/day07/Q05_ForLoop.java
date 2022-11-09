7a3c4ae425fcbf7b7e2b92f14af9cd7401df14bf
Ahmet Aycicek <aaycicek09@gmail.com>
2022-11-09 14:34:06 +0300

    new

--- src/practice/day07/Q05_ForLoop.java
+++ src/practice/day07/Q05_ForLoop.java
@@ -1,5 +1,7 @@
 package practice.day07;
 
+import java.util.Scanner;
+
 public class Q05_ForLoop {
     /* Kullanicidan sayilar girmesini isteyin
      * ve girdigi sayilari toplayip yazdirin.
@@ -9,6 +11,27 @@ public class Q05_ForLoop {
      *
      */
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+        int toplam = 0;
+        int count = 0;
+
+        for (int i=0; i<100; i++){
+            System.out.println("sayı giriniz: ");
+            int sayı= sc.nextInt();
+            count++;
+            toplam+=sayı;
+
+            if (toplam >100){
+                System.out.println(count + " kez sayı girdin yeter gari");
+                break;
+            }
+
+            System.out.println("toplam = " + toplam);
+
+        }
+
 
 
     }
