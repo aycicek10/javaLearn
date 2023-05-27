1845b390b6f42bf04ece50ddd6996f68397b0695
Ahmet Aycicek <aaycicek09@gmail.com>
2023-05-27 12:51:05 +0300

    new

---
+++ src/j37_Enum/enum01/Aylar.java
@@ -0,0 +1,7 @@
+package j37_Enum.enum01;
+
+public enum Aylar {
+
+
+
+}
---
+++ src/j37_Enum/enum01/Runner.java
@@ -0,0 +1,7 @@
+package j37_Enum.enum01;
+
+public class Runner {
+    public static void main(String[] args) {
+        //task -- verilen bir ay numarasına göre kac gün cektiğini print eden code create ediniz
+    }
+}
